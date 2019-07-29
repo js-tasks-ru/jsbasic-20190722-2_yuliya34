0fa330c6607e05172a19259010786f0f3d62aeda
Julija R-T <y.rolich@epc-it.by>
2019-07-29 23:23:02 +0300

    Add 1 task (2 lesson)

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -5,5 +5,21 @@
  * @returns {number}
  */
 function pow(m, n) {
+    var result = m;
 
+    for (var i = 1; i < n; i++) {
+        result *= m;
+    }
+
+    return result;
+}
+
+var m = prompt("Укажите число m", '');
+var n = prompt("Укажите степень n", '');
+
+if (n <= 1) {
+    alert('Некорректно указана степень, число n должно быть целым числом и больше 1'
+    );
+} else {
+    alert( pow(m, n) );
 }
