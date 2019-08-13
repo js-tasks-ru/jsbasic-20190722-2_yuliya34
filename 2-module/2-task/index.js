27654253780696e2c64c80849de4fce6baba0dec
Julija R-T <y.rolich@epc-it.by>
2019-08-13 20:52:49 +0300

    Task 2 from 2-module

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,10 +1,5 @@
-/**
- * Проверяем объект obj на пустоту
- * @param {Object} obj
- * @returns {Boolean}
- */
 function isEmpty(obj) {
-    for (let key in obj) {
+    for (var key in obj) {
         return false;
     }
     return true;
