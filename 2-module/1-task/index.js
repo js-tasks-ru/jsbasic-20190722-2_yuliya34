9ceff5d6b1179522078302e1943d4d3103ddc819
Julija R-T <y.rolich@epc-it.by>
2019-08-13 22:39:00 +0300

    Task 2 from 2-module

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,7 +1,16 @@
+/**
+ * Клонируем объект
+ * @param {Object} obj - клонируем объект
+ * @returns {Object}
+ */
 function clone(obj){
     let second = {};
-    for (var key in obj) {
-        second[key] = obj[key];
+    for (let key in obj) {
+        if (obj[key] && typeof obj[key] === 'object') {
+            second[key] = clone(obj[key]);
+        } else {
+            second[key] = obj[key];
+        }
     }
     return second;
 }
