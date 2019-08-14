26a00b574cd3f5dcaae9348cf33b2006b341b6c8
Julija R-T <y.rolich@epc-it.by>
2019-08-14 21:24:30 +0300

    3 module 1 task

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -5,4 +5,16 @@
  * @returns {string}
  */
 function showSalary(data, age) {
+    function getBy(data, age) {
+        let filteredData = data.filter(function(data) {
+            if(data.age <= age) {
+                return data.age
+            }
+        });
+        return filteredData;
+    }
+    let filteredData = getBy(data, age);
+
+    return result = filteredData.map(a => a.name + ', ' + a.balance)
+        .join(', ');
 }
