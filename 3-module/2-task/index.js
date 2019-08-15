33a315e4db2dbaa507119616745f982a9b0624a6
Julija R-T <y.rolich@epc-it.by>
2019-08-15 10:29:03 +0300

    upd 3rd module

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -13,26 +13,26 @@
         let arr2 = arr.filter(function(v){return v === +v});
 
         function arrayMin(arr2) {
-            let len = arr2.length, min = Infinity;
+            let len = arr2.length, minimum = Infinity;
             while (len--) {
-                if (Number(arr2[len]) < min) {
-                    min = Number(arr2[len]);
+                if (Number(arr2[len]) < minimum) {
+                    minimum = Number(arr2[len]);
                 }
             }
-            return min;
+            return minimum;
         }
-        let min = arrayMin(arr2);
+        let minimum = arrayMin(arr2);
 
         function arrayMax(arr2) {
-            let len = arr2.length, max = -Infinity;
+            let len = arr2.length, maximum = -Infinity;
             while (len--) {
-                if (Number(arr2[len]) > max) {
-                    max = Number(arr2[len]);
+                if (Number(arr2[len]) > maximum) {
+                    maximum = Number(arr2[len]);
                 }
             }
-            return max;
+            return maximum;
         }
-        let max = arrayMax(arr2);
+        let maximum = arrayMax(arr2);
 
-        return result = {maximum: max, minimum: min};
+        return result = {max: maximum, min: minimum};
     }
